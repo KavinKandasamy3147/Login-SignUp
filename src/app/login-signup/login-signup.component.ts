e711464ae692804a8f6435655fb63012597f38b5
Kavin Kandasamy <kavin@ascendoncloud.com>
2024-03-30 20:38:44 +0530

    initial commits

---
+++ src/app/login-signup/login-signup.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-signup',
+  templateUrl: './login-signup.component.html',
+  styleUrls: ['./login-signup.component.css']
+})
+export class LoginSignupComponent implements OnInit {
+
+  userArray:any=[];
+
+  signUpUsersObj:any={
+    userName:'',
+    emailId:'',
+    password:''
+  }
+
+  loginUsersObj:any ={
+    userName:'',
+    password:'',
+  }
+
+  signUp(){
+    this.userArray.push(this.signUpUsersObj);
+    localStorage.setItem("signUpUsers",JSON.stringify(this.userArray));
+
+  }
+
+  loginUser(){
+    let isUserExist = this.userArray.find((m:any)=>m.userName === this.loginUsersObj.userName && m.password === this.loginUsersObj.password);
+    if(isUserExist != undefined)
+    alert("user login successfully");
+    else
+    alert("login with wrong credentials")
+  }
+
+  ngOnInit(): void {
+    let localData = localStorage.getItem("signUpUsers")
+    if(localData != null)
+    this.userArray = JSON.parse(localData)
+  }
+
+}
